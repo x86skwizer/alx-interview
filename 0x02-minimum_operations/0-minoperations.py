0a63c06fb5b8b9b723419901c18e1c35acf3d2b8
x86skwizer <yassineamrire@protonmail.com>
2024-05-09 23:18:07 +0100

    minioperations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''Minimum operations coding challenge.
+'''
+
+
+def minOperations(n):
+    '''Computes the fewest number of operations.
+    '''
+    if not isinstance(n, int):
+        return 0
+    done = 1
+    ops_count = 0
+    clipboard = 0
+    while done < n:
+        if clipboard == 0:
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+        elif n - done > 0 and (n - done) % done == 0:
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+        elif clipboard > 0:
+            done += clipboard
+            ops_count += 1
+    return ops_count
